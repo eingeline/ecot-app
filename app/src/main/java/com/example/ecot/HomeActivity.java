fc51775b60de1386f133a6216023f52fd651ebc1
eingeline <eingelinenroque@gmail.com>
2024-08-19 17:14:53 +0800

    unfinished

--- app/src/main/java/com/example/ecot/HomeActivity.java
+++ app/src/main/java/com/example/ecot/HomeActivity.java
@@ -1,16 +1,29 @@
 package com.example.ecot;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class HomeActivity extends AppCompatActivity {
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
+
+
+        findViewById(R.id.transportButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), TransportActivity.class));
+            }
+        });
 
     }
 }
